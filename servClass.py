e9c15d0123f6d750e91f3eeba2611c0190529be7
Denton <163344926+cDenton1@users.noreply.github.com>
2024-12-14 21:39:56 -0700

    Delete servClass.py

--- servClass.py
+++
@@ -1,6 +0,0 @@
-# class used for storing passwords in pickle file
-class services:
-    def __init__(self, service, name, password):
-        self.service = service
-        self.name = name
-        self.password = password
